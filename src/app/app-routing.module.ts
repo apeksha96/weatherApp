028bb5fc8ee8fea6edb59ed13ea9a57aa66555d6
apeksha horambe <horambeapeksha@gmail.com>
2021-04-20 14:50:41 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CurrentComponent } from './current/current.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { SettingComponent } from './setting/setting.component';
+
+const routes: Routes = [
+  {path:'',component:CurrentComponent},
+  {path:'current',component:CurrentComponent},
+  {path:'forecast',component:ForecastComponent},
+  {path:'setting',component:SettingComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
